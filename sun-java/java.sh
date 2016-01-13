61bac0acef4a8a938f59e4219359654176f25ae4
Jack Scott <jack@nine78.com>
2016-01-13 01:07:29 -0500

    adding additional environment variables

--- sun-java/java.sh
+++ sun-java/java.sh
@@ -1,2 +1,5 @@
 export JAVA_HOME={{ java_home }}
 export PATH=$JAVA_HOME/bin:$PATH
+export LD_LIBRARY_PATH={{ java_home }}/lib:$LD_LIBRARY_PATH
+export JAVAH={{ java_home }}/bin/javah
+export JAR={{ java_home }}/bin/jar
